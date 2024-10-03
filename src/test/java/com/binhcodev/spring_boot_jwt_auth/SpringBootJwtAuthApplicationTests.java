302de49889fc8cc5312dff89febcea1cd39aadf5
BinhTX <binh.xuantran@digicommercegroup.com>
2024-10-03 11:22:51 +0700

    first commit

---
+++ src/test/java/com/binhcodev/spring_boot_jwt_auth/SpringBootJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binhcodev.spring_boot_jwt_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
